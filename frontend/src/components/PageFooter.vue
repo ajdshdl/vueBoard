0a328e71676cbf70e838f60e263c495d6266a1aa
kdh <ajdshdl@naver.com>
2023-02-28 17:34:04 +0900

    vueBoard

---
+++ frontend/src/components/PageFooter.vue
@@ -0,0 +1,17 @@
+<!-- PageFooter.vue -->
+<template>
+  <hr/>
+  <footer>
+    여기는 footer 자리입니다.
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "PageHeader"
+}
+</script>
+
+<style scoped>
+
+</style>
